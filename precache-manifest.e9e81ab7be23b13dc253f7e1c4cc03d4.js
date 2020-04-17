ac336f3e5d6f4bd49d25c1a4dfa4142ba4099a18
Gauthamjm007 <j.mgautham15@gmail.com>
2020-04-17 12:52:56 +0530

    Updates

---
+++ precache-manifest.e9e81ab7be23b13dc253f7e1c4cc03d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec34252ea60c8367f43a57068be6a893",
+    "url": "/dct_dashboard/index.html"
+  },
+  {
+    "revision": "24630df86a9ed8552ebc",
+    "url": "/dct_dashboard/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "88bd0c1eef5e6fd38433",
+    "url": "/dct_dashboard/static/js/2.d700e3bd.chunk.js"
+  },
+  {
+    "revision": "63eaea6dd422a49b51bcbe1463096476",
+    "url": "/dct_dashboard/static/js/2.d700e3bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24630df86a9ed8552ebc",
+    "url": "/dct_dashboard/static/js/main.77e7f373.chunk.js"
+  },
+  {
+    "revision": "c1e1100b0f7508f93886",
+    "url": "/dct_dashboard/static/js/runtime-main.c741fc2d.js"
+  }
+]);
